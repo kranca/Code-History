b27caa4a1086fd7973a26b1abc95abfba2e93e9c
kranca <90696292+kranca@users.noreply.github.com>
2022-01-31 09:00:47 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Raúl Carrancá on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
